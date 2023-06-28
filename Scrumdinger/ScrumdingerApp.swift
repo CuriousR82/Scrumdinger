b0d04a0fcfeea59a6e4de341cbca3278d56f0b48
CuriousR82 <rosajeon82@gmail.com>
2023-06-28 14:30:03 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Rosa Jeon on 2023-06-28.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
