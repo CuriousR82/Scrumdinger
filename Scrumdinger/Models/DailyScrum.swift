a16f118f8e829097cf87e0b0c5b2c2bed36e8165
CuriousR82 <rosajeon82@gmail.com>
2023-06-30 11:40:13 -0400

    creating a navigation hierarchy

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -10,25 +10,41 @@ import Foundation
 struct DailyScrum: Identifiable {
     // To conform to Identifiable, a model must have a property named id. Your code won’t compile until you add this property.
     let id: UUID
-        var title: String
-        var attendees: [String]
-        var lengthInMinutes: Int
-        var theme: Theme
+    var title: String
+    var attendees: [Attendee]
+    var lengthInMinutes: Int
+    var theme: Theme
     
     // Add an initializer that assigns a default value to the id property.
     // When you define a default value for a parameter, you can omit that parameter when you call an initializer or function.
+    // Initializers like this takes parameters and creates a DailyScrum struct with the parameters.
     init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
+        self.id = id
+        self.title = title
+        // This line of code initializes the attendees property of a DailyScrum instance by converting an array of strings into an array of Attendee objects. Each Attendee object is created with a unique identifier (id) and the corresponding name from the attendees array.
+        // $0 refers to the current element being processed in the closure
+        self.attendees = attendees.map { Attendee(name: $0)}
+        self.lengthInMinutes = lengthInMinutes
+        self.theme = theme
+    }
+}
+
+extension DailyScrum {
+    struct Attendee: Identifiable {
+        let id: UUID
+        var name: String
+        
+        init(id: UUID = UUID(), name: String) {
             self.id = id
-            self.title = title
-            self.attendees = attendees
-            self.lengthInMinutes = lengthInMinutes
-            self.theme = theme
+            self.name = name
         }
     }
+}
 
 extension DailyScrum {
     static var sampleData: [DailyScrum] {
         [
+            // the parameters here goes to the init function in the daily scrum struct, then creates a structure
             DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, theme: .yellow),
             DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, theme: .orange),
             DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 5, theme: .poppy)
--- Scrumdinger/Views/ScrumsView.swift
+++ Scrumdinger/Views/ScrumsView.swift
@@ -11,9 +11,24 @@ struct ScrumsView: View {
     let scrums: [DailyScrum]
     
     var body: some View {
-        List(scrums) { scrum in
-            CardView(scrum: scrum)
+        NavigationStack {
+            List(scrums) { scrum in
+                // The destination presents a single view in the navigation hierarchy when a user interacts with the element. Each row pushes to an individual destination. The Text view acts as a placeholder until you create the detail screen.
+                // takes you to another view, then CardView interacts with the user.
+                // NavigationLink(destination: Text(scrum.title)) {
+                // In ScrumsView.swift, update the NavigationLink to set DetailView as the new destination.
+                NavigationLink(destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
+                }
                 .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
         }
     }
 }
